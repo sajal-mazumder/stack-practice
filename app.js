c62cad9d8f2bbf5930b8f3a1717143169d409c6a
sajal-mazumder <sajalmazumder47@gmail.com>
2025-05-01 01:00:15 +0600

    some update

---
+++ app.js
@@ -0,0 +1,47 @@
+
+//  declaration variable
+var a = 22;
+var b = 21;
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a - b);
+// console.log(typeof(a)); this two lines return same result. 
+// console.log(typeof a);  this two lines return same result.
+
+ 
+
+
+
+// var c = '23';
+// var d = '20';
+// console.log(c + d); // output 2320 as string
+// console.log(c - d); // output 3 as number
+// console.log(c * d); // output 460 as number
+// console.log(c / d); // output 1.15 as number
+
+var c = '20';
+var d = 'sajal';
+var e;
+// console.log(c + d); // 20sajal as string result
+// console.log(c - d); // NaN
+// console.log(c * d); // NaN
+// console.log(c / d); // NaN
+// console.log(e); // undefined
+// console.log(typeof e); // undefined
+// console.log(typeof c); // string
+// console.log(Number(c)); // return as number color. convert to number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
